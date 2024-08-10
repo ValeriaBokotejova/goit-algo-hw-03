bba17dd0aecddbef15e74b9cca88c75f4db80863
tabira <valeria.bokotejova@gmail.com>
2024-08-10 16:30:28 +0200

    update task 3

--- task03.py
+++ task03.py
@@ -1,25 +1,20 @@
-# Вимоги до завдання:
-# Параметр функції phone_number - це рядок з телефонним номером у різноманітних форматах.
-# Функція видаляє всі символи, крім цифр та символу '+'.
-# Якщо міжнародний код відсутній, функція додає код '+38'. Це враховує випадки, коли номер починається з '380' (додається лише '+') 
-# та коли номер починається без коду (додається '+38').
-# Функція повертає нормалізований телефонний номер у вигляді рядка.
-
-
 import re
+
 def normalize_phone(phone_number):
-    cleaned_number = re.sub(r'\D', '', phone_number)
-    if cleaned_number.startswith('+') and len(cleaned_number) == 12:
+        
+    cleaned_number = re.sub(r'[^\d+]', '', phone_number)
+    if cleaned_number.startswith('+') and len(cleaned_number) == 13:
         return cleaned_number
-    elif len(cleaned_number) == 12:
+    elif cleaned_number.startswith('380') and len(cleaned_number) == 12:
         cleaned_number = '+' + cleaned_number
         return cleaned_number
+    elif not cleaned_number.startswith('+') and len(cleaned_number) == 10:
+        return '+38' + cleaned_number
     else:
-        cleaned_number = '+38' + cleaned_number
-        return cleaned_number
+        raise ValueError(f"Invalid phone number format: {phone_number}")
 
 
-# Приклад використання:
+# Example usage:
 raw_numbers = [
     "067\\t123 4567",
     "(095) 234-5678\\n",
@@ -29,11 +24,11 @@ raw_numbers = [
     "     0503451234",
     "(050)8889900",
     "38050-111-22-22",
-    "38050 111 22 11   ",
+    "38050 111 22 11"
 ]
 
 sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
-print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+print("Normalized phone numbers for SMS distribution:", sanitized_numbers)
 
 
 
